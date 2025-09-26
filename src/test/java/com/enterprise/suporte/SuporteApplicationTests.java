4c1469c60d89d062610feaced8dbe1b46a47fc8d
Gabriella Franco <gabriella-nena@hotmail.com>
2025-09-26 19:23:03 -0300

    Commit inicial

---
+++ src/test/java/com/enterprise/suporte/SuporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.suporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
