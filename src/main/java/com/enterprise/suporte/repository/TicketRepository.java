ae8a8645364828f48a0d504e39df3e088eae1e4f
Gabriella Franco <gabriella-nena@hotmail.com>
2025-10-03 10:46:36 -0300

    TicketController implementado

--- src/main/java/com/enterprise/suporte/repository/TicketRepository.java
+++ src/main/java/com/enterprise/suporte/repository/TicketRepository.java
@@ -1,19 +1,25 @@
 package com.enterprise.suporte.repository;
 
 import com.enterprise.suporte.enuns.TicketStatus;
+import com.enterprise.suporte.model.Customer;
 import com.enterprise.suporte.model.SupportAgent;
 import com.enterprise.suporte.model.Ticket;
-import com.enterprise.suporte.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     @Override
     Page<Ticket> findAll(Pageable pageable);
+
+    Page<Ticket> findAllByCustomerId(Long id, Pageable pageable);
+
+    Page<Ticket> findAllByAgentId(Long id, Pageable pageable);
 
     Long countByAgentResponsibleAndStatus(SupportAgent agent, TicketStatus status);
 }
